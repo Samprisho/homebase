217a1a49aa88c1aa2db3d05894887f5ae5614720
Samprisho <samuel.esho@protonmail.com>
2025-06-14 15:13:02 +0300

    We have fixed the problem!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 
-export default {
+/** */
+export default defineConfig({
   resolve: {
     alias: {
       classes: path.resolve(__dirname, "classes"),
     },
   },
-};
+  build: {
+    assetsDir: "homebase",
+  },
+});
